e2c4ce447a7a930776675fc50139a01dbb0300af
lost <perviyrazvks@gmail.com>
2024-11-29 11:35:58 +0300

    structured 1 lab

---
+++ game(2 lab)/src/main/java/org/example/Interfaces/Attacker.java
@@ -0,0 +1,9 @@
+package org.example.Interfaces;
+
+import org.example.units.Unit;
+
+public interface Attacker {
+
+	void attack(Unit unit);
+
+}
---
+++ game(2 lab)/src/main/java/org/example/buildings/MobileHouse.java
@@ -0,0 +1,9 @@
+package org.example.buildings;
+
+import org.example.Interfaces.Moveable;
+
+public class MobileHouse implements Moveable {
+
+
+
+}
